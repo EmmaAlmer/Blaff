37bd52ac0321f0db45c43935f967c584a6bffd91
EmmaAlmer <emmm1019@gmail.com>
2025-12-08 16:07:08 +0100

    första commit

---
+++ src/main/java/com/emmaalmer/Blaff/BlaffApplication.java
@@ -0,0 +1,13 @@
+package com.emmaalmer.Blaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlaffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emmaalmer/Blaff/BlaffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmaalmer.Blaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlaffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
